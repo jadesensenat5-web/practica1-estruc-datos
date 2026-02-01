056f1359ab99ed8eaf38d17f6d1f2dbf4292f75b
jadesensenat5-web <jadesensenat5@gmail.com>
2026-02-01 18:01:24 -0400

    Create Program.cs
    ejercicio10

---
+++ practica1ejercicio10/practica1ejercicio10/Program.cs
@@ -0,0 +1,23 @@
+﻿// 10.Realizar un programa que al introducir un número diga si un número es primo o no.
+
+Console.WriteLine("Introduce el primer valor:");
+int valor_1 = int.Parse(Console.ReadLine());
+
+int valor_2 = 0;
+int i = 1;
+while (i <= valor_1)
+{
+    if (valor_1 % i == 0)
+    {
+        valor_2 = valor_2 + 1;
+    }
+    i = i + 1;
+}
+if (valor_2 == 2)
+{
+    Console.WriteLine("Es Primo");
+}
+else
+{
+    Console.WriteLine("No es Primo");
+}
