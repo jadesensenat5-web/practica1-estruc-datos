b70a4106bee6308e3c671c95784c35e214aca6b9
jadesensenat5-web <jadesensenat5@gmail.com>
2026-02-01 15:44:55 -0400

    Create Program.cs
    primero1

---
+++ practica1-estruc-datos/practica1-estruc-datos/Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+//Realizar un programa que te permita introducir 2 números y muestre la suma. (Solo números enteros).
+
+Console.WriteLine("Introduce el primer valor:");
+int valor_1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Introduce el segundo valor:");
+int valor_2 = int.Parse(Console.ReadLine());
+
+int suma = valor_1 + valor_2;
+
+Console.WriteLine(suma.ToString());
