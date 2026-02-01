2e90d5248c88a77344718d74dd6b98da70c844d8
jadesensenat5-web <jadesensenat5@gmail.com>
2026-02-01 16:55:45 -0400

    Create Program.cs
    ejercicio5

---
+++ practica1ejercicio5/practica1ejercicio5/Program.cs
@@ -0,0 +1,16 @@
+﻿// //Realizar un programa que te permita introducir un nombre y una edad y decir si es adulto o menor.
+
+Console.WriteLine("Introduce el nombre:");
+string valor_1 = Console.ReadLine();
+
+Console.WriteLine("Introduce la edad:");
+int valor_2 = int.Parse(Console.ReadLine());
+
+if (valor_2 >= 18)
+{
+    Console.WriteLine("Adulto");
+}
+else
+{
+    Console.WriteLine("Menor");
+}
