b67be2abba69b21a4b5d2a05fea74e09c7b9a149
jadesensenat5-web <jadesensenat5@gmail.com>
2026-02-01 16:16:52 -0400

    Create Program.cs
    segundo

---
+++ ejercicio2/ejercicio2/Program.cs
@@ -0,0 +1,10 @@
+﻿// 2.Realizar un programa que te permita introducir 2 números y muestre la resta. (Solo números enteros).
+
+Console.WriteLine("Introduce el primer valor:");
+int valor_1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Introduce el segundo valor:");
+int valor_2 = int.Parse(Console.ReadLine());
+
+int resta = valor_1 - valor_2;
+
+Console.WriteLine(resta.ToString());
