b0b44fe0f5ccbde7422bf1bc8d51241e15979807
jadesensenat5-web <jadesensenat5@gmail.com>
2026-02-01 17:54:36 -0400

    Create Program.cs
    ejercicio9

---
+++ practica1ejercicio9/practica1ejercicio9/Program.cs
@@ -0,0 +1,17 @@
+﻿// 9.Realizar un programa que mediante un número introducido por teclado, muestre si el número es positivo (+), negativo(-) o neutro (0).
+
+Console.WriteLine("Introduce el primer valor:");
+int valor_1 = int.Parse(Console.ReadLine());
+
+if (valor_1 > 0)
+{
+    Console.WriteLine("Positivo");
+}
+else if (valor_1 < 0)
+{
+    Console.WriteLine("Negativo");
+}
+else
+{
+    Console.WriteLine("Neutro");
+}
