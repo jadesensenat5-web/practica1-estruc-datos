4daebb4a4fc581d798972ce45b0677166d0dcffc
jadesensenat5-web <jadesensenat5@gmail.com>
2026-02-01 16:26:46 -0400

    Create Program.cs
    tercero3

---
+++ practica1ejercicio3/practica1ejercicio3/Program.cs
@@ -0,0 +1,10 @@
+﻿// 3.Realizar un programa que te permita introducir 2 números y muestre la multiplicación. (Solo números enteros).
+
+Console.WriteLine("Introduce el primer valor:");
+int valor_1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Introduce el segundo valor:");
+int valor_2 = int.Parse(Console.ReadLine());
+
+int multiplicacion = valor_1 * valor_2;
+
+Console.WriteLine(multiplicacion.ToString());
