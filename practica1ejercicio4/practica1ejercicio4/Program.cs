2988c6575974fd64ff84da6ce08575837c3910e9
jadesensenat5-web <jadesensenat5@gmail.com>
2026-02-01 16:51:08 -0400

    Create Program.cs
    ejercicio4

---
+++ practica1ejercicio4/practica1ejercicio4/Program.cs
@@ -0,0 +1,11 @@
+﻿// 4.Realizar un programa que te permita introducir 2 números y muestre la división. (Incluir números enteros y decimales).
+
+Console.WriteLine("Introduce el primer valor:");
+double valor_1 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Introduce el segundo valor:");
+double valor_2 = double.Parse(Console.ReadLine());
+
+double division = valor_1 / valor_2;
+
+Console.WriteLine(division.ToString());
