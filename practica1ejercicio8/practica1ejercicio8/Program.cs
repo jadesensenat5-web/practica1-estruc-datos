076ebaee8f723beac058caa20010ba092a0f15d4
jadesensenat5-web <jadesensenat5@gmail.com>
2026-02-01 17:48:32 -0400

    Create Program.cs
    ejercicio8

---
+++ practica1ejercicio8/practica1ejercicio8/Program.cs
@@ -0,0 +1,13 @@
+﻿// 8.Realizar un programa que, mediante un número introducido por teclado, muestre si es par o impar utilizando IF y ELSE.
+
+Console.WriteLine("Introduce el primer valor:");
+int valor_1 = int.Parse(Console.ReadLine());
+
+if (valor_1 % 2 == 0)
+{
+    Console.WriteLine("Es Par");
+}
+else
+{
+    Console.WriteLine("Es Impar");
+}
