f1ecf0036698b3158fbc933e9326841633cdce2c
jadesensenat5-web <jadesensenat5@gmail.com>
2026-02-01 17:14:50 -0400

    Create Program.cs
    ejercicio7

---
+++ practica1ejercicio7/practica1ejercicio7/Program.cs
@@ -0,0 +1,13 @@
+﻿// 7.Realizar un programa que muestre los números impares del 1 al 100 utilizando WHILE.
+
+int valor_1 = 1;
+
+while (valor_1 <= 100)
+{
+    if (valor_1 % 2 != 0)
+    {
+        Console.WriteLine(valor_1.ToString());
+    }
+
+    valor_1 = valor_1 + 1;
+}
