bbfa6b891b83bfd45dd69a9b1a8fd37118373c3a
jadesensenat5-web <jadesensenat5@gmail.com>
2026-02-03 07:09:33 -0400

    Create Program.cs
    ejercicio14

---
+++ paractica1ejercicio14/paractica1ejercicio14/Program.cs
@@ -0,0 +1,13 @@
+﻿//14.Realizar un programa que calcule el factorial de un número introducido por el usuario.
+
+Console.WriteLine("Introduce el valor:");
+int valor_1 = int.Parse(Console.ReadLine());
+
+long factorial = 1;
+
+for (int i = 1; i <= valor_1; i++)
+{
+    factorial = factorial * i;
+}
+
+Console.WriteLine(factorial.ToString());
