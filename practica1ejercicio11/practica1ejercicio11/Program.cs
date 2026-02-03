ca462acfa5ac2b2db79217d936ec281c1cc9c76f
jadesensenat5-web <jadesensenat5@gmail.com>
2026-02-03 06:41:32 -0400

    Create Program.cs
    ejecicio11

---
+++ practica1ejercicio11/practica1ejercicio11/Program.cs
@@ -0,0 +1,11 @@
+﻿// 11.Realizar un programa que al introducir un número es perfecto o no.
+
+Console.WriteLine("Introduce el primer valor:");
+int valor_1 = int.Parse(Console.ReadLine());
+int suma = 0;
+for (int i = 1; i < valor_1; i++)
+{
+    if (valor_1 % i == 0) suma += i;
+}
+if (suma == valor_1) Console.WriteLine("Es Perfecto");
+else Console.WriteLine("No es Perfecto");
