05c0e1c7cb5ae188ced1deee603b07f85a0ab362
jadesensenat5-web <jadesensenat5@gmail.com>
2026-02-03 08:21:46 -0400

    Create Program.cs
    ejercicio23

---
+++ practica1ejercicio23/practica1ejercicio23/Program.cs
@@ -0,0 +1,10 @@
+﻿//23.Realizar un programa que invierta una cadena de texto.
+
+Console.WriteLine("Introduce el texto:");
+string texto = Console.ReadLine();
+string invertido = "";
+for (int i = texto.Length - 1; i >= 0; i--)
+{
+    invertido += texto[i];
+}
+Console.WriteLine(invertido);
