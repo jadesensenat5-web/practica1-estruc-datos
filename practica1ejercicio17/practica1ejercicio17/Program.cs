e1418e6d43a8d1604d0cba5f17425fd67fa5f47f
jadesensenat5-web <jadesensenat5@gmail.com>
2026-02-03 07:41:33 -0400

    Create Program.cs
    ejercicio17

---
+++ practica1ejercicio17/practica1ejercicio17/Program.cs
@@ -0,0 +1,10 @@
+﻿//17.Realizar un programa que calcule la suma de los dígitos de un número.
+
+Console.WriteLine("Introduce el valor:");
+string valor_1 = Console.ReadLine();
+int suma = 0;
+for (int i = 0; i < valor_1.Length; i++)
+{
+    suma += int.Parse(valor_1[i].ToString());
+}
+Console.WriteLine(suma.ToString());
