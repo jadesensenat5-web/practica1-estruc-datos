52b80d564b65021b429341bc3d285a8698d2b54e
jadesensenat5-web <jadesensenat5@gmail.com>
2026-02-03 07:04:53 -0400

    Create Program.cs
    ejercicio13

---
+++ practica1ejercicio13/practica1ejercicio13/Program.cs
@@ -0,0 +1,12 @@
+﻿//13.Realizar un programa que al introducir dos números (inicio y final), muestre la suma de todos sus números.
+
+Console.WriteLine("Introduce el inicio:");
+int valor_1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Introduce el final:");
+int valor_2 = int.Parse(Console.ReadLine());
+int suma = 0;
+for (int i = valor_1; i <= valor_2; i++)
+{
+    suma = suma + i;
+}
+Console.WriteLine(suma.ToString());
