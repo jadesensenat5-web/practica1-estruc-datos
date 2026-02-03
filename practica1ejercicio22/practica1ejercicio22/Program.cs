b633043d72f3e626b0e36e266494f30f6d306cb6
jadesensenat5-web <jadesensenat5@gmail.com>
2026-02-03 08:14:33 -0400

    Create Program.cs
    ejercicio22

---
+++ practica1ejercicio22/practica1ejercicio22/Program.cs
@@ -0,0 +1,7 @@
+﻿//Realizar un programa que verifique si un año introducido es bisiesto.
+
+Console.WriteLine("Introduce el año:");
+int año = int.Parse(Console.ReadLine());
+
+if (DateTime.IsLeapYear(año)) Console.WriteLine("Es Bisiesto");
+else Console.WriteLine("No es Bisiesto");
