379b2d96749bbc9aae98476a64e70ebd2519a555
jadesensenat5-web <jadesensenat5@gmail.com>
2026-02-03 08:06:21 -0400

    Create Program.cs
    ejercicio21

---
+++ practica1ejercicio21/practica1ejercicio21/Program.cs
@@ -0,0 +1,10 @@
+﻿//21.Realizar un programa que calcule el promedio de una lista de números.
+
+Console.WriteLine("¿Cuántos números quieres?");
+int n = int.Parse(Console.ReadLine());
+double suma = 0;
+for (int i = 0; i < n; i++)
+{
+    suma += double.Parse(Console.ReadLine());
+}
+Console.WriteLine("Promedio: " + (suma / n));
