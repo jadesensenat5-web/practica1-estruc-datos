ed1c32086626b107e8118474f11adf5e8b526047
jadesensenat5-web <jadesensenat5@gmail.com>
2026-02-03 07:50:45 -0400

    Create Program.cs
    ejercicio19

---
+++ practica1ejercicio19/practica1ejercicio19/Program.cs
@@ -0,0 +1,12 @@
+﻿//19.Realizar un programa que muestre los primeros N números de la serie de Fibonacci.
+
+Console.WriteLine("¿Cuántos números?");
+int n = int.Parse(Console.ReadLine());
+int a = 0, b = 1, suma;
+for (int i = 0; i < n; i++)
+{
+    Console.WriteLine(a);
+    suma = a + b;
+    a = b;
+    b = suma;
+}
