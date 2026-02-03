1cff7e7df5bea36e4118f24519ca1cde026e7d67
jadesensenat5-web <jadesensenat5@gmail.com>
2026-02-03 06:50:14 -0400

    Create Program.cs
    ejercicio12

---
+++ practica1ejercicio12/practica1ejercicio12/Program.cs
@@ -0,0 +1,10 @@
+﻿//12.Realizar un programa que al introducir dos números (inicio y final) muestre el rango entre ellos.
+
+Console.WriteLine("Introduce el inicio:");
+int valor_1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Introduce el final:");
+int valor_2 = int.Parse(Console.ReadLine());
+for (int i = valor_1; i <= valor_2; i++)
+{
+    Console.WriteLine(i.ToString());
+}
