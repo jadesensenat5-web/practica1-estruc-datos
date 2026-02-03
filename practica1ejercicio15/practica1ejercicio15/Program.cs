895649b4580348563428a4e7bb2cc024f3c31186
jadesensenat5-web <jadesensenat5@gmail.com>
2026-02-03 07:17:32 -0400

    Create Program.cs
    ejercicio15

---
+++ practica1ejercicio15/practica1ejercicio15/Program.cs
@@ -0,0 +1,11 @@
+﻿//15.Realizar un programa que determine si un número es capicúa.
+
+Console.WriteLine("Introduce el valor:");
+string valor_1 = Console.ReadLine();
+string invertido = "";
+for (int i = valor_1.Length - 1; i >= 0; i--)
+{
+    invertido += valor_1[i];
+}
+if (valor_1 == invertido) Console.WriteLine("Es Capicua");
+else Console.WriteLine("No es Capicua");
