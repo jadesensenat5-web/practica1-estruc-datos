b713e28e77d7fe6fab699f5785a7d22fa53e3929
jadesensenat5-web <jadesensenat5@gmail.com>
2026-02-03 07:45:49 -0400

    Create Program.cs
    ejercicio18

---
+++ practica1ejercicio18/practica1ejercicio18/Program.cs
@@ -0,0 +1,20 @@
+﻿//18.Realizar un programa que encuentre el mayor entre tres números introducidos por el usuario.
+
+Console.WriteLine("Introduce el primer número:");
+int valor_1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Introduce el segundo número:");
+int valor_2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Introduce el tercer número:");
+int valor_3 = int.Parse(Console.ReadLine());
+if (valor_1 >= valor_2 && valor_1 >= valor_3)
+{
+    Console.WriteLine("El mayor es: " + valor_1);
+}
+else if (valor_2 >= valor_1 && valor_2 >= valor_3)
+{
+    Console.WriteLine("El mayor es: " + valor_2);
+}
+else
+{
+    Console.WriteLine("El mayor es: " + valor_3);
+}
