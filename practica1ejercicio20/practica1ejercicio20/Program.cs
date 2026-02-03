ad58caa374f98118da7191ae6e97a769c88c4c2a
jadesensenat5-web <jadesensenat5@gmail.com>
2026-02-03 07:54:40 -0400

    Create Program.cs
    ejercicio20

---
+++ practica1ejercicio20/practica1ejercicio20/Program.cs
@@ -0,0 +1,8 @@
+﻿//20.Realizar un programa que genere la tabla de multiplicar de un número del 1 al 10.
+
+Console.WriteLine("¿Qué tabla quieres?");
+int n = int.Parse(Console.ReadLine());
+for (int i = 1; i <= 10; i++)
+{
+    Console.WriteLine(n + " x " + i + " = " + (n * i));
+}
