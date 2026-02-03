0e7ac35259200dff3f895840076909a824be9b65
jadesensenat5-web <jadesensenat5@gmail.com>
2026-02-03 07:28:59 -0400

    Create Program.cs
    ejercicio16

---
+++ practica1ejercicio16/practica1ejercicio16/Program.cs
@@ -0,0 +1,6 @@
+﻿//16.Realizar un programa que cuente los dígitos de un número introducido por el usuario.
+
+Console.WriteLine("Introduce el valor:");
+string valor_1 = Console.ReadLine();
+int cantidad = valor_1.Length;
+Console.WriteLine(cantidad.ToString());
